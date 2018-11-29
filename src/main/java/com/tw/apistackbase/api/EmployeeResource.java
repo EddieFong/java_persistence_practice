d38bbcfbeba74d06ccaecd1255a5ef99d9a6f977
Eddie Fong <fonged@oocl.com>
2018-11-29 12:08:29 +0800

    add getById and return when post

--- src/main/java/com/tw/apistackbase/api/EmployeeResource.java
+++ src/main/java/com/tw/apistackbase/api/EmployeeResource.java
@@ -1,10 +1,10 @@
 package com.tw.apistackbase.api;
 
 import com.tw.apistackbase.core.Employee;
-import com.tw.apistackbase.application.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.logging.Logger;
 
 @RestController
@@ -12,8 +12,8 @@ import java.util.logging.Logger;
 public class EmployeeResource {
     private final Logger log = Logger.getLogger(this.getClass().getName());
 
-    @Autowired
-    private EmployeeService employeeService;
+//    @Autowired
+//    private EmployeeService employeeService;
 
     @Autowired
     private EmployeeRepository employeeRepository;
@@ -21,12 +21,21 @@ public class EmployeeResource {
     @GetMapping(produces = {"application/json"})
     public Iterable<Employee> list() {
         return employeeRepository.findAll();
-//        return employeeService.list();
+    }
+
+    @GetMapping(path = "/{id}", produces = {"application/json"})
+    public Optional<Employee> list(@PathVariable Long id) {
+        return employeeRepository.findById(id);
     }
 
     @PostMapping(produces = {"application/json"})
-    public void add(@RequestBody Employee employee) {
-        employeeRepository.save(employee);
-//        employeeService.add(employee);
+    public Long add(@RequestBody Employee employee) {
+        employee = employeeRepository.save(employee);
+        return employee.getId();
     }
+
+//    @PostMapping(produces = {"application/json"})
+//    public void add(@RequestBody Employee employee) {
+//        employeeRepository.save(employee);
+//    }
 }
--- src/main/java/com/tw/apistackbase/core/Employee.java
+++ src/main/java/com/tw/apistackbase/core/Employee.java
@@ -38,4 +38,12 @@ public class Employee {
     public void setAge(int age) {
         this.age = age;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
